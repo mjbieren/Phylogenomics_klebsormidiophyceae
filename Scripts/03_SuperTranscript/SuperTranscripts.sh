ff84567feedd4ed975794bf317703b5c1bb3264e
mjbieren <107464589+mjbieren@users.noreply.github.com>
2023-12-29 23:10:49 +0100

    Update SuperTranscripts.sh

--- Scripts/03_SuperTranscript/SuperTranscripts.sh
+++ Scripts/03_SuperTranscript/SuperTranscripts.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p medium
 #SBATCH -t 2:00:00
-#SBATCH -o %J_SSuperTranscripts.out
+#SBATCH -o %J_SuperTranscripts.out
 #SBATCH -C scratch2
 #SBATCH --exclusive
 #SBATCH --mem 20G
@@ -12,5 +12,5 @@ module purge
 module load trinity
 #module load python/3.8.2 # python3 required by TRINITY (old version), not newer "trinity" 
 
-/usr/product/bioinfo/TRINITY/2.11.0/Analysis/SuperTranscripts/Trinity_gene_splice_modeler.py --trinity_fasta Trinity_Output.Trinity.fasta
+~/TRINITY/2.11.0/Analysis/SuperTranscripts/Trinity_gene_splice_modeler.py --trinity_fasta Trinity_Output.Trinity.fasta
 
